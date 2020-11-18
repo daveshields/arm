94048871ce1b2904fb78eae4893c9c9da6a9f504
dave shields <thedaveshields@gmail.com>
2020-11-18 12:02:28 -0500

    work on integer operators

--- osint/int-risc.c
+++ osint/int-risc.c
@@ -16,24 +16,47 @@ Copyright 2012-2015 David Shields
 
 void cvd__() {
 	
+	
 	reg_wa = reg_ia % 10;
 	reg_ia /= 10;
 	reg_wa  = -reg_wa + 48; // convert remainder to character code for digit
 }
 
+
 /*
  * dvi - divide into accumulator
  */
-int dvi__()
+int dvi__(int arg)
 {
-	reg_ia /= reg_w0;
+	if (arg == 0)	return EXIT_1;
+	reg_ia /= arg;
+	return EXIT_0;
 }
 
 /*
  * rmi - remainder after division into accumulator
  */
-int rmi__()
+int rmi__(int arg)
 {
-	reg_ia %= reg_w0;
+	if (arg == 0)	return EXIT_1;
+	reg_ia %= arg;
+	return EXIT_0;
 }
+
+/*
+ *  mli - multiplication
+ */
+int mli__(int arg)
+{
+	int save_arg;
+
+	save_ia = ia;
+	reg_ia *= arg;
+	if (reg_ia == 0) return EXIT_0;
+	/* check for overflow by seeing that result divided by original value of ia is arg. */
+	/* original value of ia cannot be zero since result was not zero. */
+	if(reg_ia / save_ia != arg) return EXIT_1;
+	return EXIT_0;
+}
+
 
